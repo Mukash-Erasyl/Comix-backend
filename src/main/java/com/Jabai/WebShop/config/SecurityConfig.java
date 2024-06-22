cb98d3c711dd68ca03911e9cafc510b959780fbb
Ерасыл <erasyl@MacBook-Air-Erasyl.local>
2024-06-22 13:41:40 +0500

    preDep3

--- src/main/java/com/Jabai/WebShop/config/SecurityConfig.java
+++ src/main/java/com/Jabai/WebShop/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.Jabai.WebShop.config;
 
 import com.Jabai.WebShop.filter.JwtRequestFilter;
 import com.Jabai.WebShop.util.JwtUtil;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -32,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+                .cors().and()  // Включить CORS
                 .authorizeRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/comix/createComix").authenticated()
@@ -48,13 +48,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
-
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // Configure authentication manager to use UserDetails service and password encoder
         auth.userDetailsService(inMemoryUserDetailsService()).passwordEncoder(passwordEncoder());
     }
-
 
     public JwtRequestFilter jwtRequestFilter() {
         return new JwtRequestFilter(jwtUtil);
@@ -62,13 +59,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // Password encoder used to encode passwords
         return new BCryptPasswordEncoder();
     }
 
     @Bean(name = "inMemoryUserDetailsService")
     public UserDetailsService inMemoryUserDetailsService() {
-        // Create an in-memory user for testing purposes
         UserDetails user = User.builder()
                 .username("BigLover")
                 .password(passwordEncoder().encode("sWeetheart76&"))
